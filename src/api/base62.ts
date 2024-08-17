893844036cdb75e974de6340793c0071fb9ea49c
Leonardo Gatti <leonardogattidev@gmail.com>
2024-08-17 20:08:40 -0300

    added base62 encoding/decoding

---
+++ src/api/base62.ts
@@ -0,0 +1,30 @@
+const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+export function encode(i: number): string {
+  if (i === 0) return "0"
+
+  let encoded = ""
+  const charsetLen = charset.length
+
+  while (i > 0) {
+    const char = charset[i % charsetLen]
+    encoded = char + encoded
+
+    i = Math.floor(i / charsetLen)
+  }
+
+  return encoded
+}
+
+export function decode(s: string): number {
+  let decoded = 0;
+
+  for (let i = 0; i < s.length; i += 1) {
+    const char = s[i]
+    const value = charset.indexOf(char)
+    decoded = decoded * 62 + value
+  }
+
+  return decoded
+}
+
