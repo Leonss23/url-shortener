850ad151b678ab423c4631af73cb70dbb5a5efd9
Leonardo Gatti <leonardogattidev@gmail.com>
2024-08-19 00:35:40 -0300

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "JS - setup";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = {nixpkgs, flake-utils, ...}:
+    let
+        systems = builtins.attrNames nixpkgs.legacyPackages;
+    in flake-utils.lib.eachSystem systems (
+        system: let
+            pkgs = import nixpkgs {inherit system;};
+        in 
+        # rec
+        {
+            devShells.default = pkgs.mkShell { 
+                buildInputs = with pkgs; [
+                    # nodejs_20
+                    # corepack_21
+                    # wrangler
+                    bun
+                    nodejs
+                ];
+
+                # nativeBuildInputs = with pkgs; [];
+
+            };
+        }
+    );
+}
