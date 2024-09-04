59425f04ba8243cb093a8d2f7139505823b79434
Leonardo Gatti <leonardogattidev@gmail.com>
2024-09-04 04:26:15 -0300

    repository: improved query error handling

--- src/db/repository/addUrl.ts
+++ src/db/repository/addUrl.ts
@@ -1,22 +1,38 @@
-import { LibsqlError } from "@libsql/client";
 import type { DB } from "..";
 import { ApiError } from "../../lib/ApiError";
 import { urls } from "../schema";
 import getIdByUrl from "./getIdByUrl";
 
 export default async function addUrl(db: DB, url: string) {
-  const id = await db.insert(urls).values({ url }).returning({ id: urls.id }).then((v) => v[0].id).catch(async (error) => {
-    if (error instanceof LibsqlError && error.message.includes("UNIQUE")) {
-      const id = await getIdByUrl(db, url)
-      if (id instanceof ApiError) return new ApiError(409, 'URL is already registered; Failed to get short URL')
-      return id
-    }
-    console.error(error)
-    if (error instanceof LibsqlError && error.message.includes("401")) return new ApiError(503, 'Service Unavailable. Database interaction failed')
+  const id = await db
+    .insert(urls)
+    .values({ url })
+    .returning({ id: urls.id })
+    .then((v) => v[0].id)
+    .catch(async (error) => {
+      if (!(error instanceof Error)) return ApiError.internal();
 
-    return ApiError.internal()
-  })
-  if (id instanceof ApiError) return id
+      // USER ERROR HANDLING
+      if (error.message.includes("UNIQUE")) {
+        const id = await getIdByUrl(db, url);
+        if (id instanceof ApiError)
+          return new ApiError(
+            409,
+            "URL is already registered; Failed to get short URL",
+          );
+        return id;
+      }
 
-  return id
+      // SERVER ERROR HANDLING
+      console.error(error);
+      if (error.message.includes("401"))
+        return new ApiError(
+          503,
+          "Service Unavailable. Database interaction failed",
+        );
+      return ApiError.internal();
+    });
+  if (id instanceof ApiError) return id;
+
+  return id;
 }
--- src/db/repository/getUrlById.ts
+++ src/db/repository/getUrlById.ts
@@ -1,19 +1,29 @@
 import { eq } from "drizzle-orm";
 import { urls } from "../schema";
 import type { DB } from "..";
-import { LibsqlError } from "@libsql/client";
 import { ApiError } from "../../lib/ApiError";
 
-export default async function getUrlById(db: DB, id: number): Promise<string | ApiError> {
-  const result = await db.query.urls.findFirst({ columns: { url: true }, where: eq(urls.id, id) }).catch((error) => {
-    if (error instanceof LibsqlError && error.message.includes("401")) return new ApiError(503, 'Service Unavailable. Database interaction failed')
+export default async function getUrlById(
+  db: DB,
+  id: number,
+): Promise<string | ApiError> {
+  const result = await db.query.urls
+    .findFirst({ columns: { url: true }, where: eq(urls.id, id) })
+    .catch((error) => {
+      if (!(error instanceof Error)) return ApiError.internal();
 
-    console.error(error)
-    return ApiError.internal()
-  })
-  if (result instanceof ApiError) return result
+      if (error.message.includes("401"))
+        return new ApiError(
+          503,
+          "Service Unavailable. Database interaction failed",
+        );
 
-  if (!result) return new ApiError(404, 'URL not found')
+      console.error(error);
+      return ApiError.internal();
+    });
+  if (result instanceof ApiError) return result;
 
-  return result.url
+  if (!result) return new ApiError(404, "URL not found");
+
+  return result.url;
 }
