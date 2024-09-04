72744f7f0510f111c4af50ecdff34f0dd1696db4
Leonardo Gatti <leonardogattidev@gmail.com>
2024-09-04 04:44:03 -0300

    fixed missing import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,22 +1,23 @@
 import { createClient } from "@libsql/client/web";
 import { drizzle, LibSQLDatabase } from "drizzle-orm/libsql";
-import * as schema from "./schema"
+import * as schema from "./schema";
+import type { Env } from "../env";
 
-export type DB = LibSQLDatabase<typeof schema>
+export type DB = LibSQLDatabase<typeof schema>;
 
-let db: DB
+let db: DB;
 
 export function dbConnect(env: Env) {
-  if (db) return db
+  if (db) return db;
 
   const url = env.TURSO_URL?.trim();
   if (url == undefined) throw new Error("TURSO_URL env var is not defined");
   const authToken = env.TURSO_AUTH_TOKEN?.trim();
-  if (authToken == undefined) throw new Error("TURSO_AUTH_TOKEN env var is not defined");
+  if (authToken == undefined)
+    throw new Error("TURSO_AUTH_TOKEN env var is not defined");
 
-  const client = createClient({ url, authToken })
-  db = drizzle(client, { schema })
+  const client = createClient({ url, authToken });
+  db = drizzle(client, { schema });
 
-  return db
+  return db;
 }
-
