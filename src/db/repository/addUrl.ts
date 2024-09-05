20b77f0db8a8a0fb40341886b9f524ece433ac0a
Leonardo Gatti <leonardogattidev@gmail.com>
2024-09-05 01:53:30 -0300

    reduced `/api/urls` endpoint queries to 1

--- src/db/repository/addUrl.ts
+++ src/db/repository/addUrl.ts
@@ -1,27 +1,16 @@
 import type { DB } from "..";
 import { ApiError } from "../../lib/ApiError";
 import { urls } from "../schema";
-import getIdByUrl from "./getIdByUrl";
 
 export default async function addUrl(db: DB, url: string) {
   const id = await db
     .insert(urls)
     .values({ url })
+    .onConflictDoUpdate({ target: urls.url, set: { url: url } })
     .returning({ id: urls.id })
     .then((v) => v[0].id)
     .catch(async (error) => {
       if (!(error instanceof Error)) return ApiError.internal();
-
-      // USER ERROR HANDLING
-      if (error.message.includes("UNIQUE")) {
-        const id = await getIdByUrl(db, url);
-        if (id instanceof ApiError)
-          return new ApiError(
-            409,
-            "URL is already registered; Failed to get short URL",
-          );
-        return id;
-      }
 
       // SERVER ERROR HANDLING
       console.error(error);
