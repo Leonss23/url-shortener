dbcac6bfef1d0987c8e4c29f7ab0a19919084e9e
Leonardo Gatti <leonardogattidev@gmail.com>
2024-09-04 23:00:39 -0300

    improved feedback message for "Shorten URL" form

--- src/components/UrlForm.tsx
+++ src/components/UrlForm.tsx
@@ -1,80 +1,87 @@
-/** @jsxImportSource solid-js */
-
-import { createSignal } from "solid-js";
-import type { DOMElement } from "solid-js/jsx-runtime";
+import { createResource, createSignal, Match, Switch } from "solid-js";
 
 export default function UrlForm() {
+  const [url, setUrl] = createSignal<string>();
+  const [shortUrl] = createResource(url, fetchLink);
+
   let input!: HTMLInputElement;
 
-  const [shortUrl, setShortUrl] = createSignal("");
-  const [error, setError] = createSignal("");
-
-  async function handleSubmit(
-    e: Event & { submitter: HTMLElement } & {
-      currentTarget: HTMLFormElement;
-      target: DOMElement;
-    },
-  ) {
-    e.preventDefault();
-    const url = input.value;
-    const registeredUrl = await registerURL(url);
-    if (!registeredUrl) return setError("Failed to register URL");
-    setShortUrl(registeredUrl);
-  }
-
   return (
-    <>
-      <div class="sm:text-xl md:text-2xl flex flex-col items-center container">
-        <form
-          onSubmit={handleSubmit}
-          class="shadow bg-white rounded-lg overflow-hidden flex justify-between w-fit"
-        >
-          <div>
-            <input
-              class="p-2 outline outline-red-600 sm:min-w-80 md:min-w-96"
-              type="url"
-              value="https://leonardo-gatti.pages.dev"
-              ref={input}
-            />
-          </div>
-          <button class="p-2 bg-indigo-800 text-white hover:bg-indigo-700 active:bg-indigo-600 duration-50">
-            Shorten
-          </button>
-        </form>
-        <div class="p-4 font-medium">
-          <p
-            class="border p-2 border-green-400 bg-green-50 rounded-lg"
-            classList={{ invisible: !shortUrl() }}
-          >
-            Short URL:{" "}
-            <a href={shortUrl()} class="text-indigo-600">
-              {shortUrl()}{" "}
-            </a>
-          </p>
-          <p
-            class="p-2 border border-red-400 rounded-lg bg-red-50"
-            classList={{ invisible: !error() }}
-          >
-            <span class="text-red-500">Error: </span> {error()}
-          </p>
+    <div class="sm:text-xl md:text-2xl flex flex-col items-center container">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setUrl(input.value);
+        }}
+        class="shadow bg-white rounded-lg overflow-hidden flex justify-between w-fit"
+      >
+        <div>
+          <input
+            class="p-2 outline outline-red-600 sm:min-w-80 md:min-w-96"
+            type="url"
+            value="https://leonardo-gatti.pages.dev"
+            ref={input}
+          />
         </div>
+        <button
+          disabled={shortUrl.loading}
+          class="p-2 bg-indigo-800 text-white hover:bg-indigo-700 active:bg-indigo-600 duration-50 disabled:bg-neutral-600"
+        >
+          Shorten
+        </button>
+      </form>
+      <div class="p-4 font-medium [&>*]:p-2 [&>*]:border [&>*]:rounded-lg">
+        <Switch fallback={<div class="invisible">fallback</div>}>
+          <Match when={shortUrl.loading}>
+            <div class="border-neutral-400 bg-neutral-200">Hold on...</div>
+          </Match>
+          <Match when={shortUrl.state == "errored"}>
+            <div class="border-red-400 bg-red-50">
+              {shortUrl.error.toString()}
+            </div>
+          </Match>
+          <Match when={shortUrl.state == "ready"}>
+            <div class="border-green-400 bg-green-50 ">
+              Shortened URL:{" "}
+              <a href={shortUrl()} class="text-indigo-600 font-bold">
+                {shortUrl()}
+              </a>
+            </div>
+          </Match>
+        </Switch>
       </div>
-    </>
+    </div>
   );
 }
 
-async function registerURL(url: string) {
-  try {
-    const data = await fetch("/api/urls", {
-      body: JSON.stringify({ url }),
-      headers: { "content-type": "application/json" },
-      method: "post",
-    }).then((v) => v.json());
-    const { shortUrl }: any = data;
-    if (typeof shortUrl != "string")
-      throw new Error("failed to parse url from response");
-    return shortUrl;
-  } catch (error) {
-    console.error({ error });
+const fetchLink = async (url: string) => {
+  const response = await fetch(new URL("/api/urls", window.location.href), {
+    body: JSON.stringify({ url }),
+    headers: {
+      "content-type": "application/json",
+      accept: "application/json",
+    },
+    method: "post",
+  });
+  if (!response.ok) {
+    throw new Error(await getErrorMessage(response), {
+      cause: response,
+    });
+  }
+  const json = await response.json();
+  console.log({ json });
+  return json.shortUrl as string;
+};
+
+async function getErrorMessage(response: Response): Promise<string> {
+  const contentType = response.headers.get("content-type")!;
+
+  switch (contentType) {
+    case "application/json":
+      return (await response.json()).error as string;
+    case "text/plain":
+      return await response.text();
+    default:
+      return response.statusText;
   }
 }
